5b6f22da8954aa260cf6b7688ac2ae284cfc2924
Karen-Mtz <dkareen.mtz@gmail.com>
2019-10-26 13:40:20 -0500

    projecto diccionario de emojis lista con funciones para editar y eliminar

---
+++ Week5/Prework/EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Apple Device 18 on 10/26/19.
+//  Copyright © 2019 Tonipocket. All rights reserved.
+//
+
+import Foundation
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
